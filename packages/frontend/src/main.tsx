98a13c53fffc04f83d0e6c113dceaf24ac0c5379
Jess Graham <jess@jessgraham.me>
2022-06-25 21:28:21 -0400

    New configuration for heroku deployment

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import ApolloClientProvider from './provider/ApolloClientProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloClientProvider>
+      <App />
+    </ApolloClientProvider>
   </React.StrictMode>,
 );
---
+++ packages/frontend/src/provider/ApolloClientProvider.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const ApolloClientProvider: React.FC<Props> = ({ children }) => {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default ApolloClientProvider;
